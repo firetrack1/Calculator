39fbb6370bf3ca21552f239f72d01e1cd6db5e7f
firetrack1 <107789018+firetrack1@users.noreply.github.com>
2023-08-22 18:37:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,22 +12,24 @@ while True:
     op = input("Enter operator (+,-,/,*): ")
     num1 = int(input("Enter first number: "))
     num2 = int(input("Enter second number: "))
+    
+    try:
+    	if op == "+":
+        	print("Result: " + str(add(num1, num2)))
+    	elif op == "-":
+        	print("Result: " + str(subtract(num1, num2)))
+    	elif op == "/":
+        	print("Result: " + str(divide(num1, num2)))
+    	elif op == "*":
+        	print("Result: " + str(multiply(num1, num2)))
+    	else:
+        	print(op + " is not a valid operator.")
+    except ZeroDivisionError:
+        print("Cannot divide by zero.")
+        
+    finally:
+    	restart = input("Restart the program? [y]/[n]: ")
 
-    if op == "+":
-        print("Result: " + str(add(num1, num2)))
-    elif op == "-":
-        print("Result: " + str(subtract(num1, num2)))
-    elif op == "/":
-        try:
-            print("Result: " + str(divide(num1, num2)))
-        except ZeroDivisionError:
-            print("You cannot divide by zero.")
-    elif op == "*":
-        print("Result: " + str(multiply(num1, num2)))
-    else:
-        print(op + " is not a valid operator.")
 
-    restart = input("Restart the program? [y]/[n]: ")
-
-    if restart == "n":
+        if restart == "n":
         exit()
